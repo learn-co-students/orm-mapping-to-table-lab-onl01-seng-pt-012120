86f694574ed40ea031cc267ef25572e86de0c166
Derric Fragosa <fderric@gmail.com>
2020-04-06 01:38:59 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -4,35 +4,36 @@ class Student
   attr_reader :id 
   
   def initialize(id = nil, name, grade)
+    @id = id
     @name = name
     @grade = grade
-    @id = id 
+     
   end 
 
   def self.create_table
   sql = <<-SQL
         CREATE TABLE students (id INTEGER PRIMARY KEY,
         name TEXT, grade TEXT);
-        SQL 
+        SQL
         DB[:conn].execute(sql)
-  end
+  end 
 
   def self.drop_table
-    sql = <<-SQL 
-      DROP TABLE students;
-      SQL 
-      DB[:conn].execute(sql)
+    sql = <<-SQL
+    DROP TABLE students;
+    SQL
+    DB[:conn].execute(sql)
   end
   
   def save
-    sql = <<-SQL
+    sql = <<-SQL 
       INSERT INTO students(name, grade)
       VALUES (?,?);
-    SQL 
+    SQL
     DB[:conn].execute(sql, self.name, self.grade)
     @id = DB[:conn].execute("SELECT MAX(ID) AS LastID FROM students")[0][0]
   end
-  
+
   def self.create(name:, grade:)
     student = Student.new(name, grade)
     student.save
